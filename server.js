23991aaef3ec1d9be8de478d799acb100b9501d1
David Farr <david.farr@sap.com>
2015-04-23 12:34:09 -0700

    Create basic time regex and stub out flow

--- server.js
+++ server.js
@@ -5,12 +5,61 @@ var slack = new Slack(token, true, true);
 
 slack.on('message', function(message) {
     var user = slack.getUserByID(message.user);
- 	var channel = slack.getChannelGroupOrDMByID(message.channel);
+    var channel = slack.getChannelGroupOrDMByID(message.channel);
 
-    if (message.type === 'message' 
-    	&& channel.name === 'bottesting') {
-        console.log(channel.name + ' ' + user.name + ':' + message.text);
-    	channel.send('Hey there');
+    // if (message.type === 'message' 
+    //     && channel.name === 'bottesting') {
+    //     console.log(channel.name + ' ' + user.name + ':' + message.text);
+    //     channel.send('Hey there');
+    // }
+
+    var regex = /([0-9]{1,2}):([0-9]{2})(am|pm)?/gi;
+
+    var match = regex.exec(message);
+
+    console.log(match);
+
+    if(match && message.type === 'message' && channel.name === 'bottesting') {
+
+        var time = new Date();
+
+        var hour = parseInt(match[1], 10);
+        var mins = parseInt(match[2], 10);
+
+        if(match[3] && match[3].toLowerCase() === 'pm') {
+            hour = hour + 12;
+        }
+
+        time.setHours(hour);
+        time.setMinutes(mins);
+
+        channel.send(time);
+
+        // User.findOne({uuid: message.user}, function(err, user) {
+
+        //     if(err) {
+        //         return;
+        //     }
+
+        //     if(!user.timezone) {
+        //         // ask user for timezone
+        //         return;
+        //     }
+
+        //     channel.users.forEach(function (user) {
+                
+        //         User.findOne({uuid: user.id}, function(err, user) {
+
+        //             if(!err && user.timezone) {
+        //                 channel.sendDM(time, user.id);
+        //             }
+
+        //         });
+
+        //     });
+
+        // });
+
     }
 });
  
