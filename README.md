9ac78003e32dc9ef851cf2c90fa94c9f9e29d751
Asche <chris.asche@gmail.com>
2015-04-23 21:52:34 -0400

    removed regex parsing, added chrono. Added moment and moment timezone to do fun time stuff

--- README.md
+++ README.md
@@ -1,6 +1,19 @@
-# fathertime
+# Fathertime
 
 A slack bot that converts time to your local timezone.
+
+## Time
+
+Fathertime can interpret any Time [Chrono](https://github.com/wanasit/chrono) can.
+
+Basically: 
+
+* Today, Tomorrow, Yesterday, Last Friday, etc
+* 17 August 2013 - 19 August 2013
+* This Friday from 13:00 - 16.00
+* 5 days ago
+* Sat Aug 17 2013 18:40:39 GMT+0900 (JST)
+* 2014-11-30T08:15:30-05:30
 
 # Environment
 
--- server.js
+++ server.js
@@ -2,38 +2,46 @@ var config = require('./config.js');
 var Slack = require('slack-client');
 var token = config.slackApiKey;
 var slack = new Slack(token, true, true);
+var chrono = require('chrono-node');
+var m = require('moment');
+var tz = require('moment-timezone');
 
 slack.on('message', function(message) {
     var user = slack.getUserByID(message.user);
     var channel = slack.getChannelGroupOrDMByID(message.channel);
+    var results = chrono.parse(message.text);
 
-    // if (message.type === 'message' 
-    //     && channel.name === 'bottesting') {
-    //     console.log(channel.name + ' ' + user.name + ':' + message.text);
-    //     channel.send('Hey there');
-    // }
+    if (results.length > 0 && message.type === 'message' && channel.name === 'bottesting') {
+    	results.forEach(function(result) {
+    		var start = m(result.start.date());
+    		channel.send(start.format('lll'));
+    		console.log(start.format('lll'));
 
-    var regex = /([0-9]{1,2}):([0-9]{2})(am|pm)?/gi;
+    		if (result.end) {
+    			var end = m(result.end.date());
+    			channel.send(end.format('lll'));
+    			console.log(end.format('lll'));
+    		}
+    	});
+    }
 
-    var match = regex.exec(message);
+    // var regex = /([0-9]{1,2}):([0-9]{2})(am|pm)?/gi;
+    // var match = regex.exec(message);
+    // if (match && message.type === 'message' && channel.name === 'bottesting') {
 
-    console.log(match);
+        // var time = new Date();
 
-    if(match && message.type === 'message' && channel.name === 'bottesting') {
+        // var hour = parseInt(match[1], 10);
+        // var mins = parseInt(match[2], 10);
 
-        var time = new Date();
+        // if (match[3] && match[3].toLowerCase() === 'pm') {
+        //     hour = hour + 12;
+        // }
 
-        var hour = parseInt(match[1], 10);
-        var mins = parseInt(match[2], 10);
+        // time.setHours(hour);
+        // time.setMinutes(mins);
 
-        if(match[3] && match[3].toLowerCase() === 'pm') {
-            hour = hour + 12;
-        }
-
-        time.setHours(hour);
-        time.setMinutes(mins);
-
-        channel.send(time);
+        // channel.send(time);
 
         // User.findOne({uuid: message.user}, function(err, user) {
 
@@ -60,7 +68,7 @@ slack.on('message', function(message) {
 
         // });
 
-    }
+    // }
 });
  
 slack.login();
